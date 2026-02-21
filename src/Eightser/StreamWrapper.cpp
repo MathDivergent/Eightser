bbaa6338e321a2d0dc89a7b0511e854874e53e00
MathDivergent <71389444+MathDivergent@users.noreply.github.com>
2026-02-21 19:50:42 +0200

    Update src/Eightser/StreamWrapper.cpp

--- src/Eightser/StreamWrapper.cpp
+++ src/Eightser/StreamWrapper.cpp
@@ -1,9 +1,11 @@
 #include <Eightser/StreamWrapper.hpp>
 
 extern template class ::eightser::wrapper::ibyte_stream_t<>;
-extern template class ::eightser::wrapper::obyte_stream_t<>;
-
 #ifdef EIGHTSER_FILESTREAM_ENABLE
 extern template class ::eightser::wrapper::ifile_stream_t<>;
+#endif // EIGHTSER_FILESTREAM_ENABLE
+
+extern template class ::eightser::wrapper::obyte_stream_t<>;
+#ifdef EIGHTSER_FILESTREAM_ENABLE
 extern template class ::eightser::wrapper::ofile_stream_t<>;
 #endif // EIGHTSER_FILESTREAM_ENABLE
